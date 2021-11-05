562d9ca36b8cc8907b24321c250bc43479472e8d
prabhupatil123 <93571477+prabhupatil123@users.noreply.github.com>
2021-11-05 19:49:19 +0530

    new upload

---
+++ wordcounter.js
@@ -0,0 +1,15 @@
+let textbox = document.getElementById("textbox");
+
+textbox.addEventListener('input', function(){
+    var text = this.value;
+    let char = text.length;
+    document.getElementById("char").innerHTML = char;
+
+    text = text.trim();
+    let words = text.split(" ");
+    let cleanlist = words.filter(function(ele){
+        return ele != "";
+
+    })
+    document.getElementById("word").innerHTML = cleanlist.length;
+})
